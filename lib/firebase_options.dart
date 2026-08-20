040b1d4397e8698aa5b69f0f19bda9f9ce10f63f
AlexBSCM <AlexBSCM@users.noreply.github.com>
2026-08-20 13:35:34 +0500

    feat: migrate to real Firebase cloud backend (geo-messenger-56544)

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,11 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 
 const firebaseOptions = FirebaseOptions(
-  apiKey: 'local-emulator',
-  appId: 'local-emulator',
-  messagingSenderId: 'local-emulator',
-  projectId: 'geo-messenger-local',
-  authDomain: 'localhost',
-  databaseURL: 'http://localhost:8080',
-  storageBucket: 'localhost',
+  apiKey: 'AIzaSyDWUe2WqjW8Xz0of35_TbFOZkvrZ4eaN6A',
+  appId: '1:1040603826276:android:0d682e9d8095ff3b7a3771',
+  messagingSenderId: '1040603826276',
+  projectId: 'geo-messenger-56544',
+  authDomain: 'geo-messenger-56544.firebaseapp.com',
+  storageBucket: 'geo-messenger-56544.firebasestorage.app',
 );
